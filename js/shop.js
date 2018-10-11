48e0bbe76fbebe6207f5ac51968994e4ac770092
Tomasz Pleśniak <31666201+Tomke997@users.noreply.github.com>
2018-10-11 21:14:36 +0200

    get specific product

--- js/shop.js
+++ js/shop.js
@@ -27,10 +27,9 @@ function postAddProducts(post) {
     $("#product_area").append(
         CreateProduct(post));
 }
-
 function CreateProduct(post) {
     var product =
-        "<div id='"+post.id+"' class=\"product\">\n" +
+        "<div id='"+post.id+"' class=\"product\" >\n" +
         "<div id=produkt_image>\n" +
         "<img height='250' src="+post.imageLink+" alt=\"\"> \n" +
         "</div>\n" +
@@ -47,7 +46,7 @@ function CreateProduct(post) {
         "<p>"+post.price+"</p>\n" +
         "</div>\n" +
         "<div id=\"product_buy\">\n" +
-        "<p><a href="+post.id+">SHOW MORE</a>\n" +
+        "<p><a onclick='setId("+post.id+")'>SHOW MORE</a>\n" +
         "</p>\n" +
         "</div>\n" +
         "</div>\n" +
@@ -55,35 +54,11 @@ function CreateProduct(post) {
     return product;
 }
 
-/*
-function loadProductPage(post) {
-    var product =
-        "<section id=\"product_area\"> +
-        "<div class=\"product\"> +
-        "<div id=\"product_img\"> +
-        "<img src="+post.imageLink+" alt=\"\"> +
-        "</div> +
-        <div id="product_title">
-        <h2>ESCAPE YOUR CHILD</h2>
-    </div>
-    <div id="product_desc">
-        <p>
-        Good quality shit stairs fresh from the labour camps in China, only worked on by Apple approved child workers.
-    <br><br>
-    This stair in particularly excells in escaping the sticky fingers of your disgusting child, the steep design will<br> either have them fall down or not be able to follow you, for the parent who is sick of their child.
-    </p>
-    </div>
-    <div id="product_shop_buy">
-        <div id="product_price">
-        <p>PRICE:2.000.000$</p>
-    </div>
-    <div id="product_buy">
-        <p><a href="product_info.html">ADD TO CART</a>
-    </p>
-    </div>
-    </div>
-    </div>
-    </section>
-    }
-    */
-
+var globalVariable;
+function setId(id) {
+    localStorage.setItem("vOneLocalStorage", id);
+     globalVariable={
+        x: id
+    };
+    window.location.href ='product_info.html';
+}
