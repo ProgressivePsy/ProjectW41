b08a78fea05ec84a0dd1ee5fb068778d68587a7f
Tomasz Pleśniak <31666201+Tomke997@users.noreply.github.com>
2018-10-11 18:03:49 +0200

    Products in store

--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,8 @@
+$(window).on('load', function() {
+    postList();
+});
+
 function postList() {
-    // Call Web API to get a list of post
     $.ajax({
         url: 'https://stairsprojectproduction.azurewebsites.net/api/products',
         type: 'GET',
@@ -14,44 +17,44 @@ function postList() {
 }
 
 function postListSuccess(posts) {
-    // Iterate over the collection of data
+
     $.each(posts, function (index, post) {
-        // Add a row to the post table
+
         postAddProducts(post);
     });
 }
 
 function postAddProducts(post) {
-    // Check if <tbody> tag exists, add one if not
-    if ($("#postTable tbody").length == 0) {
-        $("#postTable").append("<tbody></tbody>");
-    }
-    // Append row to <table>
-    $("#product_area ul").append(
+
+
+    $("#product_area").append(
         CreateProduct(post));
 }
 
 function CreateProduct(post) {
-    "<div id=\"\"+post.id+\"\>\n" +
-    "        <div id=\"product_img\">\n" +
-    "          <img src=\"product1.jpg\" alt=\"\"> \n" +
-    "        </div>\n" +
-    "        <div id=\"product_title\">\n" +
-    "          <h2>Fucking Good Shit Stairs</h2>\n" +
-    "        </div>\n" +
-    "        <div id=\"product_desc\">\n" +
-    "          <p>\n" +
-    "            Good quality shit stairs fresh from the labour camps in China, only worked on by Apple approved child workers.\n" +
-    "          </p>\n" +
-    "        </div>\n" +
-    "        <div id=\"product_shop_buy\">\n" +
-    "          <div id=\"product_price\">\n" +
-    "            <p>PRICE:2.000.000$</p>\n" +
-    "          </div>\n" +
-    "          <div id=\"product_buy\">\n" +
-    "            <p><a href=\"#\">ADD TO CART</a>\n" +
-    "            </p>\n" +
-    "          </div>\n" +
-    "        </div>\n" +
-    "      </div>"
+    var product =
+        "<div id='"+post.id+"' class=\"product\">\n" +
+        "<div id=produkt_image>\n" +
+        "<img height='250' src="+post.imageLink+" alt=\"\"> \n" +
+        "</div>\n" +
+        "<div id=\"product_title\">\n" +
+        "<h2>"+post.name+"</h2>\n" +
+        "</div>\n" +
+        "<div id=\"product_desc\">\n" +
+        "<p>\n"+
+          post.desc+"\n" +
+        "</p>\n" +
+        "</div>\n" +
+        "<div id=\"product_shop_buy\">\n" +
+        "<div id=\"product_price\">\n" +
+        "<p>"+post.price+"</p>\n" +
+        "</div>\n" +
+        "<div id=\"product_buy\">\n" +
+        "<p><a href=\"#\">ADD TO CART</a>\n" +
+        "</p>\n" +
+        "</div>\n" +
+        "</div>\n" +
+        "</div>";
+
+    return product;
 }
